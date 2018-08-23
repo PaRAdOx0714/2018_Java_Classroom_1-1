32ad4a34b683e504a00aba92f71af580e7b025c9
user <user@JBTC10117>
2018-08-23 15:04:47 +0900

    02차시

---
+++ 자바기초실습/src/제02차시/AppMain.java
@@ -0,0 +1,15 @@
+package 제02차시;
+
+class AppMain {
+	
+	public static void main(String[] args) {
+		System.out.println("main() : I'm main() function!");
+		System.out.println("main() : I'm the start of Java program!");
+		
+		new Class01();
+		
+		System.out.println("System : End of program.");
+		
+	}
+
+}
---
+++ 자바기초실습/src/제02차시/Class01.java
@@ -0,0 +1,25 @@
+package 제02차시;
+
+class Class01 {
+	int i=0;
+	
+	Class01(){
+		i+=3;
+		System.out.println("Class01 : I'm a creater of Class01 class.");
+		System.out.println("Class01 : I called just after you made an objecct shape of Class01.");
+		System.out.println("i : I'm i, a.k.a field or member variable. Just know me as a public variable at C.");
+		System.out.println("Current i : " + i);
+		
+		doIt();
+		
+	}
+	
+	void doIt() {
+		i+=5;
+		System.out.println("doIt() : I'm doIt() method, a.k.a member function.");
+		System.out.println("doIt() : Call me whenever you need me. I'll be right there.");
+		System.out.println("i : I'm i. How do you call me name? Oh, common, buddy. Field or member variable.");
+		System.out.println("Current i : " + i);
+	}
+
+}
