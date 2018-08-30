309b7c28d8601357c9686bc5e45023439259b04e
user <user@JBTC10117>
2018-08-30 15:29:56 +0900

    제03차시

--- 자바기초실습/src/제03차시/실습01.java
+++ 자바기초실습/src/제03차시/실습01.java
@@ -1,17 +1,19 @@
 package 제03차시;
 
-public class 실습01 {
-	public static void main(String[] args) {
+public class 실습01 
+{
+	public static void main(String[] args) 
+	{
 		byte a1=-128, a2=127, a3=037, a4=0x1f, a5=0b11111;
 		short b1=-32768, b2=32767;
 		int c1=-2147483648, c2=2147483647;
-		long d1=2147483649L, d2=1L<<63, d3=(1L<<63)-1;
+		long d1=2147483648L, d2=1L<<63, d3=(1L<<63)-1;
 		
 		float e=3.14f;
 		double f1=3.14, f2=3.14d;
 		
 		char g1='통';
-		char[]h1= {'통','신','과'}, h2= "통신과".toCharArray();
+		char[]h1= {'통','신','과'}, h2= "통신과".toCharArray();		//.toCharArray => "통신과"(즉, String 객체)의 멤버 
 		char h3[] = {'통','신','과'}, h4[]="통신과".toCharArray();
 		
 		boolean i=true;
--- 자바기초실습/src/제03차시/실습02.java
+++ 자바기초실습/src/제03차시/실습02.java
@@ -1,5 +1,20 @@
 package 제03차시;
 
 public class 실습02 {
+	public static void main(String[] args) {
+		String a = "통신과";
+		String b[] = {"Incheon","Electronic","Meister","Highschool"};
+		
+		String c[] = new String[4];
+		c[0]="Incheon";
+		c[1]="Electronic";
+		c[2]="Meister";
+		c[3]="Highschool";
+		
+		System.out.println(a.hashCode() + " : " + a);
+		System.out.println(b.hashCode() + " : " + b[0] + " " + b[1] + " " + b[2] + " " + b[3]);
+		System.out.println(c.hashCode() + " : " + c[0] + " " + c[1] + " " + c[2] + " " + c[3]);
+
+	}
 
 }
