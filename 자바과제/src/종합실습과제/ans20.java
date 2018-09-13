205cf86d28473ca153afbe4f72fe7c009991cb48
user <user@JBTC10117>
2018-09-13 15:21:18 +0900

    whdgkq

--- 자바과제/src/종합실습과제/ans20.java
+++ 자바과제/src/종합실습과제/ans20.java
@@ -1,9 +1,18 @@
 package 종합실습과제;
 
+import java.util.Scanner;
+
 public class ans20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("주민등록번호 12자리: ");
+		String a = sc.nextLine();
+		int tmp=0;
+		for(i=0;i<=11;i++) {
+			tmp+=a.
+		}
+		
 
 	}
 
